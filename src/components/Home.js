bc1c6086770851786913fd3e504d4e3a791aa323
Toheeb Osundare <toheebosundare@gmail.com>
2022-08-22 22:35:49 +0200

    init

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,11 @@ const Home = () => {
     >
       <h2 >Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <motion.button animate={{}}>
+        <motion.button whileHover={{
+          scale: 1.1,
+          textShadow: '0px 0px 8px rgb(255,255,255)',
+          boxShadow: '0px 0px 8px rgb(255,255,255)',
+        }}>
           Create Your Pizza
         </motion.button>
       </Link>
