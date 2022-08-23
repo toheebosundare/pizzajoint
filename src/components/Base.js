1538cdb55a2a35045d429fc46efdbfd348912b14
Toheeb Osundare <toheebosundare@gmail.com>
2022-08-23 23:21:26 +0200

    init

--- src/components/Base.js
+++ src/components/Base.js
@@ -2,14 +2,38 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw'
+  },
+  visible: {
+    opacity: 1, 
+    x: 0,
+    transition:{ type: 'spring', stiffness: 120 }
+  }
+}
+
+const nextVariants = {
+  hidden: {
+    opacity: 0,
+    x: '-100vw'
+  },
+  visible: {
+    opacity: 1,
+    x:0,
+    transition: {type: 'spring', stiffness: 120}
+  }
+}
+
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
   return (
     <motion.div className="base container"
-      initial={{ x: '100vw' }}
-      animate={{ x: 0 }}
-      transition={{ type: 'spring', stiffness: 120 }}
+      variants = {containerVariants}
+      initial = 'hidden'
+      animate = 'visible'
     >
 
       <h3>Step 1: Choose Your Base</h3>
@@ -29,9 +53,8 @@ const Base = ({ addBase, pizza }) => {
 
       {pizza.base && (
         <motion.div className="next"
-          initial={{ x:'100vw'}}
-          animate={{ x: 0 }}
-          transition={{ type: 'spring', stiffness: 120 }}
+          variants={nextVariants}
+          
         >
           <Link to="/toppings">
             <motion.button
